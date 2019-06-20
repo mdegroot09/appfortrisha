f40950c380cbbdd7cddc22a13adb951aa82587e4
Mike De Groot <mdegroot09@gmail.com>
2019-06-20 07:28:52 -0600

    fixed dots bug

--- src/Components/Navbar/Dots.js
+++ src/Components/Navbar/Dots.js
@@ -6,7 +6,7 @@ export default class Dots extends Component {
     this.state = {
       navTopWidth: 0,
       dotsArr: [
-       'dotLeft1', 'dotLeft2', 'dotLeft3', 'dotLeft4', 'dotLeft5', 'dotLeft6', 'dotLeft7', 'dotLeft8', 'dotLeft9', 'dotLeft10', 'dotLeft11', 'dotLeft12', 'dotLeft13', 'dotLeft14', 'dotLeft15', 'dotLeft16', 'dotLeft17', 'dotLeft18', 'dotLeft19', 'dotLeft20', 'dotLeft21', 'dotLeft22', 'dotLeft23', 'dotLeft24', 'dotLeft25', 'dotLeft26', 'dotLeft27', 'dotLeft28', 'dotLeft29', 'dotLeft30', 'dotLeft31', 'dotLeft32', 'dotLeft33', 'dotLeft34', 'dotLeft35', 'dotLeft36', 'dotLeft37', 'dotLeft38', 'dotLeft39', 'dotLeft40'
+       'dotLeft1', 'dotLeft2', 'dotLeft3', 'dotLeft4', 'dotLeft5', 'dotLeft6', 'dotLeft7', 'dotLeft8', 'dotLeft9', 'dotLeft10', 'dotLeft11', 'dotLeft12', 'dotLeft13', 'dotLeft14', 'dotLeft15', 'dotLeft16', 'dotLeft17', 'dotLeft18', 'dotLeft19', 'dotLeft20', 'dotLeft21', 'dotLeft22', 'dotLeft23', 'dotLeft24', 'dotLeft25', 'dotLeft26', 'dotLeft27', 'dotLeft28', 'dotLeft29', 'dotLeft30', 'dotLeft31', 'dotLeft32', 'dotLeft33', 'dotLeft34', 'dotLeft35', 'dotLeft36', 'dotLeft37', 'dotLeft38'
       ]
     }
   }
@@ -20,28 +20,26 @@ export default class Dots extends Component {
   }
 
   animDots = (navTopWidth) => {
-    // console.log('lastInArr:', lastLeftArr, typeof(lastLeftArr))
     setInterval(() => { 
       var {dotsArr} = this.state
-      let lastLeftArr = parseInt(dotsArr[39].split('dotLeft')[1])
-      let lastRightArr = parseInt(dotsArr[39].split('dotRight')[1])
+      let lastLeftArr = parseInt(dotsArr[37].split('dotLeft')[1])
+      let lastRightArr = parseInt(dotsArr[37].split('dotRight')[1])
       if (lastLeftArr){
-        if (lastLeftArr < 40){
+        if (lastLeftArr < 38){
           dotsArr.push(`dotLeft${lastLeftArr + 1}`)
         } else {
           dotsArr.push(`dotRight1`)
         }
       } else if (lastRightArr){
-          if (lastRightArr < 40){
+          if (lastRightArr < 38){
             dotsArr.push(`dotRight${lastRightArr + 1}`)
           } else {
             dotsArr.push(`dotLeft1`)
           }
       }
       dotsArr.shift()
-
       this.setState({dotsArr})
-    }, 50);
+    }, 15);
   }
   
   render(){
