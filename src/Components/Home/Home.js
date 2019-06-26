286c89dc5af9a932106120d16861eab16db34586
Mike De Groot <mdegroot09@gmail.com>
2019-06-26 13:43:59 -0600

    updated home layout, styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,6 +8,11 @@ export default class Home extends Component {
           <img className='headerIcon' src="https://i1.wp.com/www.justpinkaboutit.com/wp-content/uploads/2019/04/kisscc0-half-of-a-yellow-sun-computer-icons-sun-icon-5b3dfe2b88c833.0121921515307894195603-1.png?ssl=1" alt=""/>
           <h1 className='headerTitle'>Simple Joys</h1>
         </div>
+        <div className='homeTrioDiv'>
+          <div className='homeLeft'>left</div>
+          <div className='homeMiddle'>middle</div>
+          <div className='homeRight'>right</div>
+        </div>
       </>
     )
   }
