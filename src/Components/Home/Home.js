3fd8291a97a9cab84d7378ca190897e18eca419e
Mike De Groot <mdegroot09@gmail.com>
2019-06-27 11:44:41 -0600

    updated posts styling

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,23 +6,28 @@ export default class Home extends Component {
     this.state = {
       posts: [
         {
-          title: 'Last Post',
+          title: 'Latest Post',
+          date: '6/27/19',
           text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero possimus rem, repellendus quas veniam exercitationem repudiandae voluptatum doloribus doloremque mollitia. Rerum vero sunt ad saepe nam aperiam ipsum deserunt quo.'
         },
         {
           title: 'Fourth Post',
+          date: '6/26/19',
           text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, excepturi dolore fugiat amet perferendis, doloribus quaerat reprehenderit unde a nostrum deserunt maxime. Voluptatum suscipit nisi vero molestias officiis similique animi.'
         },
         {
           title: 'Third Post',
+          date: '6/24/19',
           text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit pariatur, impedit officia ut voluptates modi eos molestiae ducimus corrupti, minima voluptatem quae, repellendus distinctio culpa aliquid in. Labore, hic esse.'
         },
         {
           title: 'Second Post',
+          date: '6/23/19',
           text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tenetur maiores fugit sapiente doloribus ipsa esse, adipisci ratione quaerat aut incidunt sit asperiores alias, consequatur reiciendis, autem quibusdam iusto dolorum!'
         },
         {
           title: 'First Post',
+          date: '6/21/19',
           text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At, harum laudantium magnam natus aliquam, excepturi, cum quaerat asperiores aperiam enim labore. Nobis in dolorum itaque illum odio assumenda nisi aliquid!'
         }
       ]
@@ -33,7 +38,7 @@ export default class Home extends Component {
     let showPosts = this.state.posts.map((post, i) => {
       return (
         <div className='postDiv' key={i}>
-          <h4>{post.title}</h4>
+          <h4 className='postTitle'>{post.title} - {post.date}</h4>
           <p className='postText'>{post.text}</p>
         </div>
       )
