f3bcce774d2197823ddb69e376804008794c696b
Mike De Groot <mdegroot09@gmail.com>
2019-07-24 13:25:17 -0600

    added image upload frontend

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
+import axios from 'axios';
 
 class NewPost extends Component {
+  submitImg = () => {
+    let photo = document.getElementById("image-file").files[0];
+    let formData = new FormData();
+    formData.append("photo", photo);
+    console.log('formData:', formData, 'photo:', photo)
+    // axios.post('/api/newImg', formData);
+  }
+  
   render(){
     return(
       <div className='homeMainDiv'>
         NewPost
+        <input id="image-file" type="file" />
+        <button onClick={this.submitImg}>Submit</button>
       </div>
     )
   }
