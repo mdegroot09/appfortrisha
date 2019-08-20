518025dfe25dabfee7692776c1807e7e56cd4b82
Mike De Groot <mdegroot09@gmail.com>
2019-08-20 13:11:44 -0600

    fixed createPost controller bugs switching to .then and returning an object

--- server/controllers/postCtrl.js
+++ server/controllers/postCtrl.js
@@ -7,7 +7,6 @@ module.exports = {
   },
 
   createPost: async (req, res) => {
-    console.log('req.body:', req.body, 'typeof:', typeof(req.body.family))
     let {postTitle, imageMain, date, family, makeup, food, elements} = req.body
     let textArr = elements.map(element => {
       if (element.text){
@@ -22,17 +21,26 @@ module.exports = {
     let postsArr = await db.postCtrl.createPost({postTitle, family, makeup, food, date, imageMain, text})
     let post_id = postsArr[0].id
 
-    elements.map(async (element) => {
-      let {type, text, quote, person, url, url2} = element
+    createElement = (i) => {
+      let {type, text, quote, person, url, url2} = elements[i]
       if (!text) {text = ''} 
       if (!quote) {quote = ''}
       if (!person) {person = ''}
-      if (!person) {person = ''}
       if (!url) {url = ''}
       if (!url2) {url2 = ''}
-      return await db.postCtrl.createElement({type, text, quote, person, url, url2, post_id})
-    })
+      db.postCtrl.createElement({type, text, quote, person, url, url2, post_id})
+      .then(e => {
+        if (i + 1 < elements.length){
+          createElement(i + 1)
+        } else return
+      })
+      .catch(err => {
+        res.sendStatus(err)
+      })
+    }
+
+    createElement(0)
     
-    res.sendStatus(200)
+    res.status(201).send({post_id})
   }
 }
