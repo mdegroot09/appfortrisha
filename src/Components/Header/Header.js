4e61e9de4f0962db22c5941e0597df2fea21974d
Mike De Groot <mdegroot09@gmail.com>
2019-08-19 19:00:50 -0600

    updated Header icon location and filter

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header () {
   return(
     <div className='headerDiv'>
-      <img className='headerIcon' src="https://www.rawshorts.com/freeicons/wp-content/uploads/2017/01/blue_spacepict10_1484336621-1.png" alt=""/>
+      <img className='headerIcon' src="https://simplejoys.s3.us-east-2.amazonaws.com/simplejoysyellow-1566262672906.png" alt=""/>
       <div className='headerTitle'>Simple Joys
         <span id='subtitleFull'>Celebrating the little things</span>
         <span id='subtitlePart1'>Celebrating the</span>
