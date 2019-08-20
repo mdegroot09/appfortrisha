99e0cb53ce7bd61801cdbbcf7d18e8b5adc5d7f0
Mike De Groot <mdegroot09@gmail.com>
2019-08-19 19:51:17 -0600

    added createPost query, updated createPost controller method

--- db/postCtrl/createPost.sql
+++ db/postCtrl/createPost.sql
@@ -1,1 +1,19 @@
-insert into posts
+insert into posts (
+  title,
+  family,
+  makeup,
+  food, 
+  datetime,
+  imagemain,
+  text
+) values (
+  ${postTitle},
+  ${family},
+  ${makeup},
+  ${food},
+  ${date},
+  ${imageMain},
+  ${text}
+)
+
+returning *;
--- server/controllers/postCtrl.js
+++ server/controllers/postCtrl.js
@@ -8,8 +8,22 @@ module.exports = {
 
   createPost: async (req, res) => {
     console.log('req.body:', req.body)
+    let {postTitle, imageMain, date, family, makeup, food, elements} = req.body
+    let textArr = elements.map(element => {
+      if (element.text){
+        return element.text + ' '
+      } else {
+        return ''
+      }
+    })
+    let text = textArr.join('').trim()
+
     const db = req.app.get('db')
-    // let postsArr = await db.postCtrl.createPost()
+    let postsArr = await db.postCtrl.createPost({postTitle}, {family}, {makeup}, {food}, {date}, {imageMain}, {text})
+    let post_id = postsArr[0].id
+    console.log('post_id:', post_id)
+    
+    
     res.sendStatus(200)
   }
 }
