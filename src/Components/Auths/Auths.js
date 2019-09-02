def27fa83bb1747e2c5d00dfc995bcd94a7077f2
Mike De Groot <mdegroot09@gmail.com>
2019-09-02 07:44:34 -0600

    updated login alert styling, duration

--- src/Components/Auths/Auths.js
+++ src/Components/Auths/Auths.js
@@ -53,7 +53,7 @@ class Auths extends Component{
       loginSuccess.style.display = 'inline'
       setTimeout(() => {
         loginSuccess.style.display = 'none'
-      }, 8000);
+      }, 6000);
       axios.post('/auth/checkID', user)
       .then(response => {
         console.log('isAdmin TRUE.', response)
@@ -68,7 +68,7 @@ class Auths extends Component{
       loginFail.style.display = 'inline'
       setTimeout(() => {
         loginFail.style.display = 'none'
-      }, 8000);
+      }, 6000);
       console.log('err:', err)
     })
   }
